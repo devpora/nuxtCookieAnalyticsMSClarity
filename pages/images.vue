fccbcedb71213026cdf78701969f2debb7c3eee8
Admin Devpora <admin@devpora.com>
2024-07-03 22:20:41 +0200

    Initial commit

---
+++ pages/images.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+const items = [
+  '/images/devpora_img_1.webp',
+  '/images/devpora_img_2.webp',
+  '/images/devpora_img_3.webp',
+  '/images/devpora_img_4.webp',
+  '/images/devpora_img_5.webp',
+  '/images/devpora_img_6.webp',
+  '/images/devpora_img_7.webp',
+  '/images/devpora_img_8.webp',
+  '/images/devpora_img_9.webp'
+]
+
+const isOpen = ref(false)
+const currentImage = ref(null)
+
+function openModal(image: string) {
+  isOpen.value = true;
+  currentImage.value = image;
+}
+
+function closeModal() {
+  isOpen.value = false;
+  currentImage.value = null;
+}
+
+useSeoMeta({
+  titleTemplate: 'Images',
+  title: 'Nuxt3 | Test Images for MS Clarity and Analytics',
+  ogTitle: 'Nuxt3 | Test Images for MS Clarity and Analytics',
+  description: 'Test image display using Nuxt3 combined with MS Clarity and Google Analytics. Ideal for developers looking for practical examples.',
+  ogDescription: 'Test image display using Nuxt3 combined with MS Clarity and Google Analytics. Ideal for developers looking for practical examples.',
+  ogImage: '/images/devpora-logo.png',
+})
+</script>
+<template>
+  <UPage>
+    <div class="grid grid-cols-3 gap-12">
+      <div
+          v-for="(image, index) in items"
+          :key="index"
+          class="relative cursor-pointer"
+      >
+        <ULandingCard
+            color="primary"
+            orientation="vertical"
+        >
+          <img
+              :src="image"
+              class="w-full rounded-md px-12 "
+              @click="openModal(image)"
+          />
+        </ULandingCard>
+      </div>
+    </div>
+
+    <UModal v-model="isOpen">
+      <div>
+        <img
+            :src="currentImage"
+            class="w-full rounded-md"
+            @click="closeModal()"
+        />
+      </div>
+
+    </UModal>
+  </UPage>
+</template>
---
+++ pages/text.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import {sendEventToAnalytics} from "~/utils/analytics";
+const cookieAnalytics = useCookie('cookieAnalytics')
+
+const text = ref(null)
+const textSecond = ref(null)
+const data = ref(null)
+const toast = useToast()
+
+function showToast(){
+  data.value = text.value ? text.value : 'Test input'
+  toast.add({ title: data.value })
+
+  // Send data to Analytics
+  if (cookieAnalytics.value === true) {
+    sendEventToAnalytics('input_test_text', {
+      event_category: 'InputField',
+      event_label: 'SubmitFirstButton',
+      value: data.value,
+    });
+  }
+}
+function showSecondToast(){
+  toast.add({ title: textSecond.value, color: 'amber' })
+
+  // Send data to Analytics
+  if (cookieAnalytics.value === true) {
+    sendEventToAnalytics('input_test_password', {
+      event_category: 'InputField',
+      event_label: 'SubmitSecondButton',
+      value: textSecond.value,
+    });
+  }
+}
+useSeoMeta({
+  titleTemplate: 'Text',
+  title: 'Nuxt3 | Test Text Input for MS Clarity and Analytics',
+  ogTitle: 'Nuxt3 | Test Text Input for MS Clarity and Analytics',
+  description: 'Test interactions with text inputs using Nuxt3 combined with MS Clarity and Google Analytics. Perfect for developers looking for practical examples.',
+  ogDescription: 'Test interactions with text inputs using Nuxt3 combined with MS Clarity and Google Analytics. Perfect for developers looking for practical examples.',
+  ogImage: '/images/devpora-logo.png',
+})
+</script>
+<template>
+  <UPage>
+    <UPageBody class="relative flex flex-col justify-center overflow-hidden " style="height: calc(100vh - 128px);">
+      <ULandingHero
+          :title="data ? data : 'My test Text'"
+          :description="`You can open the Developer tools and Network tab to check if the cookie and analytics are working correctly. Cookie for Analytics is ${cookieAnalytics ? 'enabled' : 'disabled'}. Sending data is ${cookieAnalytics ? 'allowed' : 'blocked'}`"
+      >
+        <UInput v-model="text" type="text" placeholder="Enter text and press enter" @keyup.enter="showToast"/>
+        <UInput v-model="textSecond" type="password" placeholder="Enter text and press enter" @keyup.enter="showSecondToast"/>
+      </ULandingHero>
+    </UPageBody>
+  </UPage>
+</template>
